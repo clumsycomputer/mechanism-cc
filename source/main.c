0d647eeb716e4a18bc653fc269128eacff20a827
rnd-hajb3 <rnd.hajb3@proton.me>
2024-08-02 23:00:07 +0000

    refactor(main.c): write_png_image

--- source/main.c
+++ source/main.c
@@ -30,32 +30,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void write_png_image(int pixel_width, int pixel_height, png_bytep pixel_row_ptrs, FILE* output_file_ptr) {
+    png_structp png_struct_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    png_infop png_info_ptr = png_create_info_struct(png_struct_ptr);
+    png_init_io(png_struct_ptr, output_file_ptr);
+    png_set_IHDR(png_struct_ptr, png_info_ptr, pixel_width, pixel_height, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+    png_write_info(png_struct_ptr, png_info_ptr);
+    png_write_image(png_struct_ptr, pixel_row_ptrs);
+    png_write_end(png_struct_ptr, NULL);
+    png_destroy_write_struct(&png_struct_ptr, &png_info_ptr);
+}
+
+void rasterize_points() {}
+
 int main()
 {
-    int width = 256;
-    int height = 256;
-    png_bytep image_data = (png_byte *)malloc(width * height * 3);
-    png_bytep row_pointers[height];
-    for (int y = 0; y < height; y++)
+    int max_pixel_channel_size = 255;
+    int pixel_byte_count = 3;
+    int pixel_width = 256;
+    int pixel_height = 256;
+    png_bytep pixel_data_ptr = (png_byte *)malloc(pixel_width * pixel_height * pixel_byte_count);
+    png_bytep pixel_row_ptrs[pixel_height];
+    for (int pixel_row_index = 0; pixel_row_index < pixel_height; pixel_row_index++)
     {
-        row_pointers[y] = image_data + y * width * 3;
-        for (int x = 0; x < width; x++)
+        int row_initial_pixel_offset = pixel_row_index * pixel_width * pixel_byte_count;
+        pixel_row_ptrs[pixel_row_index] = pixel_data_ptr + row_initial_pixel_offset;
+        for (int pixel_column_index = 0; pixel_column_index < pixel_width; pixel_column_index++)
         {
-            row_pointers[y][x * 3] = (png_byte)(x % 255);           // Red
-            row_pointers[y][x * 3 + 1] = (png_byte)(y % 255);       // Green
-            row_pointers[y][x * 3 + 2] = (png_byte)((x + y) % 255); // Blue
+            int pixel_index_offset = row_initial_pixel_offset + pixel_column_index * pixel_byte_count;
+            int pixel_red_byte_index = pixel_index_offset;
+            int pixel_green_byte_index = pixel_index_offset + 1;
+            int pixel_blue_byte_index = pixel_index_offset + 2;
+            pixel_data_ptr[pixel_red_byte_index] = (png_byte)(pixel_column_index % max_pixel_channel_size);
+            pixel_data_ptr[pixel_green_byte_index] = (png_byte)(pixel_row_index % max_pixel_channel_size);
+            pixel_data_ptr[pixel_blue_byte_index] = (png_byte)((pixel_column_index + pixel_row_index) % max_pixel_channel_size);
         }
     }
-    FILE *fp = fopen("image.png", "wb");
-    png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    png_infop info_ptr = png_create_info_struct(png_ptr);
-    png_init_io(png_ptr, fp);
-    png_set_IHDR(png_ptr, info_ptr, width, height, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
-    png_write_info(png_ptr, info_ptr);
-    png_write_image(png_ptr, row_pointers);
-    png_write_end(png_ptr, NULL);
-    png_destroy_write_struct(&png_ptr, &info_ptr);
-    fclose(fp);
-    free(image_data);
+    FILE *image_file_ptr = fopen("foo.png", "wb");
+    write_png_image(pixel_width, pixel_height, pixel_row_ptrs, image_file_ptr);
+    fclose(image_file_ptr);
+    free(pixel_data_ptr);
     return 0;
 }
