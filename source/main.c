fa8d2ae0106a8ca93a48515c9f3ab34fc0fcd9ff
rnd-hajb3 <rnd.hajb3@proton.me>
2024-08-08 18:39:01 +0000

    feature(main.c): fill image background

--- source/main.c
+++ source/main.c
@@ -30,45 +30,99 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void write_png_image(int pixel_width, int pixel_height, png_bytep pixel_row_ptrs, FILE* output_file_ptr) {
+typedef int Rgb8bitPixelWidth;
+typedef int Rgb8bitPixelHeight;
+typedef png_bytep Rgb8bitPixelData;
+typedef struct {
+    png_byte red;
+    png_byte green;
+    png_byte blue;
+} Rgb8bitPixelColor;
+
+#define RGB_8BIT_PIXEL_BYTE_COUNT 3
+#define RGB_8BIT_PIXEL_WIDTH_SIZE sizeof(Rgb8bitPixelWidth)
+#define RGB_8BIT_PIXEL_HEIGHT_SIZE sizeof(Rgb8bitPixelHeight)
+#define RGB_8BIT_PIXEL_WIDTH_INDEX (RGB_8BIT_PIXEL_HEIGHT_SIZE + RGB_8BIT_PIXEL_WIDTH_SIZE)
+#define RGB_8BIT_PIXEL_HEIGHT_INDEX (RGB_8BIT_PIXEL_HEIGHT_SIZE)
+
+int *rgb_8bit_pixel_data_pixel_width_ptr__(png_bytep rgb_8bit_pixel_data) {
+    return rgb_8bit_pixel_data - RGB_8BIT_PIXEL_WIDTH_INDEX;
+}
+
+int *rgb_8bit_pixel_data_pixel_height_ptr__(png_bytep rgb_8bit_pixel_data) {
+    return rgb_8bit_pixel_data - RGB_8BIT_PIXEL_HEIGHT_INDEX;
+}
+
+int rgb_8bit_pixel_width__(png_bytep rgb_8bit_pixel_data) {
+    return *rgb_8bit_pixel_data_pixel_width_ptr__(rgb_8bit_pixel_data);
+}
+
+int rgb_8bit_pixel_height__(png_bytep rgb_8bit_pixel_data) {
+    return *rgb_8bit_pixel_data_pixel_height_ptr__(rgb_8bit_pixel_data);
+}
+
+png_bytep create_rgb_8bit_pixel_data(int pixel_width, int pixel_height)
+{
+    char *rgb_8bit_pixel_data_result = (char *)malloc(pixel_width * pixel_height * RGB_8BIT_PIXEL_BYTE_COUNT + RGB_8BIT_PIXEL_WIDTH_SIZE + RGB_8BIT_PIXEL_HEIGHT_SIZE);
+    rgb_8bit_pixel_data_result = rgb_8bit_pixel_data_result + RGB_8BIT_PIXEL_WIDTH_SIZE + RGB_8BIT_PIXEL_HEIGHT_SIZE;
+    *rgb_8bit_pixel_data_pixel_width_ptr__(rgb_8bit_pixel_data_result) = pixel_width;
+    *rgb_8bit_pixel_data_pixel_height_ptr__(rgb_8bit_pixel_data_result) = pixel_height;
+    return (png_byte *)rgb_8bit_pixel_data_result;
+}
+
+void fill_rgb_8bit_pixel_data(png_bytep rgb_8bit_pixel_data, Rgb8bitPixelColor fill_color) {
+    for (int pixel_row_index = 0; pixel_row_index < rgb_8bit_pixel_height__(rgb_8bit_pixel_data); pixel_row_index++)
+    {
+        int row_initial_pixel_offset = pixel_row_index * rgb_8bit_pixel_width__(rgb_8bit_pixel_data) * RGB_8BIT_PIXEL_BYTE_COUNT;
+        for (int pixel_column_index = 0; pixel_column_index < rgb_8bit_pixel_width__(rgb_8bit_pixel_data); pixel_column_index++)
+        {
+            int pixel_index_offset = row_initial_pixel_offset + pixel_column_index * RGB_8BIT_PIXEL_BYTE_COUNT;
+            int pixel_red_byte_index = pixel_index_offset;
+            int pixel_green_byte_index = pixel_index_offset + 1;
+            int pixel_blue_byte_index = pixel_index_offset + 2;
+            rgb_8bit_pixel_data[pixel_red_byte_index] = fill_color.red;
+            rgb_8bit_pixel_data[pixel_green_byte_index] = fill_color.green;
+            rgb_8bit_pixel_data[pixel_blue_byte_index] = fill_color.blue;
+        }
+    }
+}
+
+void write_rgb_8bit_png_image(png_bytep rgb_8bit_pixel_data, FILE *output_file_ptr)
+{
+    int png_bit_depth = 8;
     png_structp png_struct_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
     png_infop png_info_ptr = png_create_info_struct(png_struct_ptr);
+    png_bytep pixel_rows_ptr[rgb_8bit_pixel_height__(rgb_8bit_pixel_data)];
+    for (int row_index = 0; row_index < rgb_8bit_pixel_height__(rgb_8bit_pixel_data); row_index++)
+    {
+        pixel_rows_ptr[row_index] = &(rgb_8bit_pixel_data[row_index * rgb_8bit_pixel_width__(rgb_8bit_pixel_data) * RGB_8BIT_PIXEL_BYTE_COUNT]);
+    }
     png_init_io(png_struct_ptr, output_file_ptr);
-    png_set_IHDR(png_struct_ptr, png_info_ptr, pixel_width, pixel_height, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+    png_set_IHDR(png_struct_ptr, png_info_ptr, rgb_8bit_pixel_width__(rgb_8bit_pixel_data), rgb_8bit_pixel_height__(rgb_8bit_pixel_data), png_bit_depth, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
     png_write_info(png_struct_ptr, png_info_ptr);
-    png_write_image(png_struct_ptr, pixel_row_ptrs);
+    png_write_image(png_struct_ptr, pixel_rows_ptr);
     png_write_end(png_struct_ptr, NULL);
     png_destroy_write_struct(&png_struct_ptr, &png_info_ptr);
 }
 
-void rasterize_points() {}
+void free_rgb_8bit_pixel_data(png_bytep rgb_8bit_pixel_data)
+{
+    free(rgb_8bit_pixel_data_pixel_width_ptr__(rgb_8bit_pixel_data));
+}
 
 int main()
 {
-    int max_pixel_channel_size = 255;
-    int pixel_byte_count = 3;
+    char *output_file_name = "foo.png";
+    int max_pixel_channel_value = (int)sizeof(png_byte) * 255;
     int pixel_width = 256;
     int pixel_height = 256;
-    png_bytep pixel_data_ptr = (png_byte *)malloc(pixel_width * pixel_height * pixel_byte_count);
-    png_bytep pixel_row_ptrs[pixel_height];
-    for (int pixel_row_index = 0; pixel_row_index < pixel_height; pixel_row_index++)
-    {
-        int row_initial_pixel_offset = pixel_row_index * pixel_width * pixel_byte_count;
-        pixel_row_ptrs[pixel_row_index] = pixel_data_ptr + row_initial_pixel_offset;
-        for (int pixel_column_index = 0; pixel_column_index < pixel_width; pixel_column_index++)
-        {
-            int pixel_index_offset = row_initial_pixel_offset + pixel_column_index * pixel_byte_count;
-            int pixel_red_byte_index = pixel_index_offset;
-            int pixel_green_byte_index = pixel_index_offset + 1;
-            int pixel_blue_byte_index = pixel_index_offset + 2;
-            pixel_data_ptr[pixel_red_byte_index] = (png_byte)(pixel_column_index % max_pixel_channel_size);
-            pixel_data_ptr[pixel_green_byte_index] = (png_byte)(pixel_row_index % max_pixel_channel_size);
-            pixel_data_ptr[pixel_blue_byte_index] = (png_byte)((pixel_column_index + pixel_row_index) % max_pixel_channel_size);
-        }
-    }
-    FILE *image_file_ptr = fopen("foo.png", "wb");
-    write_png_image(pixel_width, pixel_height, pixel_row_ptrs, image_file_ptr);
+    Rgb8bitPixelColor fill_color = {.red = 255, .green = 128, .blue = 0};
+
+    FILE *image_file_ptr = fopen(output_file_name, "wb");
+    png_bytep rgb_8bit_pixel_data = create_rgb_8bit_pixel_data(pixel_width, pixel_height);
+    fill_rgb_8bit_pixel_data(rgb_8bit_pixel_data, fill_color);
+    write_rgb_8bit_png_image(rgb_8bit_pixel_data, image_file_ptr);
+    free_rgb_8bit_pixel_data(rgb_8bit_pixel_data);
     fclose(image_file_ptr);
-    free(pixel_data_ptr);
     return 0;
 }
