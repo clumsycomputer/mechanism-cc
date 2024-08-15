4b16b23597d6fc3d52ba105d87ed508095ba0699
jared mathews <clumsycomputer@gmail.com>
2024-08-15 00:44:05 +0000

    feature(ImagePixels): initial work building out ImagePixels api

---
+++ source/ImagePixels.c
@@ -0,0 +1,19 @@
+#include "types.h"
+
+typedef enum {
+    PixelsChannelCount_RGB = 3,
+    PixelsChannelCount_RGBA = 4,
+} PixelsChannelCount;
+
+typedef enum {
+    PixelsBitDepth_8 = 8,
+    PixelsBitDepth_16 = 16
+} PixelsBitDepth;
+
+HeapAllocation __createImagePixels(U8 pixelsChannelCount, U8 pixelsBitDepth, U16 pixelsWidth, U16 pixelsHeight)
+{
+}
+
+void foo() {
+    __createImagePixels(-1, -1, 18, 18);
+}
---
+++ source/types.h
@@ -0,0 +1,16 @@
+#include <stdint.h>
+
+typedef uint8_t U8;
+typedef int8_t S8;
+typedef uint16_t U16;
+typedef int16_t S16;
+typedef uint32_t U32;
+typedef int32_t S32;
+typedef uint64_t U64;
+typedef int64_t S64;
+typedef float IEEE32;
+typedef double IEEE64;
+
+typedef char *String;
+typedef U8 Byte;
+typedef Byte *HeapAllocation;
