75a6003139c75dfa11c0c87b8ee46fc96f5356a2
Jared Mathews <refectjam@gmail.com>
2024-09-10 18:28:50 +0000

    progress

--- source/main.c
+++ source/main.c
@@ -1,13 +1,14 @@
 #include "PngEncoding.h"
 #include "PngPixels.h"
 #include <malloc.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
 {
-  U32 pixelsWidth = 512;
-  U32 pixelsHeight = 512;
+  U32 pixelsWidth = 64 + 1;
+  U32 pixelsHeight = 64 + 1;
   U64 pixelsSize =
     sizeofRgb8bitPngPixels(pixelsWidth, pixelsHeight);
   U64 maxEncodingSize =
@@ -38,9 +39,37 @@ int main(void)
           pixelRowIndex,
           pixelColumnIndex);
       currentPixelChannels_ptr->red = 255;
-      currentPixelChannels_ptr->green = 0;
+      currentPixelChannels_ptr->green = 79;
       currentPixelChannels_ptr->blue = 0;
     }
+  }
+  U32 circlePointCount = 512;
+  IEEE64 circlePointRadius = 0.9;
+  U32 originPointColumnIndex = pngPixels->width / 2;
+  U32 originPointRowIndex = pngPixels->height / 2;
+  U32 columnViewDimensionMagnitude = pngPixels->width - originPointColumnIndex - 1;
+  U32 rowViewDimensionMagnitude = pngPixels->height - originPointRowIndex - 1;
+  for (U32 pointIndex = 0; pointIndex < circlePointCount; pointIndex++)
+  {
+    IEEE64 pointAngle =
+      2 * M_PI / circlePointCount * pointIndex;
+    IEEE64 pointCos =
+      cos(pointAngle);
+    IEEE64 pointSin =
+      sin(pointAngle);
+    U32 pointColumnIndex =
+      (U32)(circlePointRadius * columnViewDimensionMagnitude * pointCos) + originPointColumnIndex;
+    U32 pointRowIndex =
+      (U32)(circlePointRadius * rowViewDimensionMagnitude * pointSin) + originPointRowIndex;
+    currentPixelChannels_ptr =
+      atPixelsDataPixelChannels(
+        pngPixels,
+        pointRowIndex,
+        pointColumnIndex);
+    currentPixelChannels_ptr->red = 0;
+    currentPixelChannels_ptr->green = 0;
+    currentPixelChannels_ptr->blue = 0;
+  // }
   }
   encodeRgb8bitPngPixels(
     pngEncoding,
