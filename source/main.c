ffe471d17c3376d2d1f938f784c94cdefeed074b
Jared Mathews <refectjam@gmail.com>
2024-09-11 00:42:00 +0000

    progress

--- source/main.c
+++ source/main.c
@@ -5,10 +5,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct
+{
+  IEEE32 originX;
+  IEEE32 originY;
+  IEEE32 magnitude;
+  Rgb8bitPixelChannels color;
+} Cell;
+
 int main(void)
 {
-  U32 pixelsWidth = 64 + 1;
-  U32 pixelsHeight = 64 + 1;
+  U32 pixelsWidth = 64;
+  U32 pixelsHeight = 64;
   U64 pixelsSize =
     sizeofRgb8bitPngPixels(pixelsWidth, pixelsHeight);
   U64 maxEncodingSize =
@@ -43,32 +51,77 @@ int main(void)
       currentPixelChannels_ptr->blue = 0;
     }
   }
-  U32 circlePointCount = 512;
-  IEEE64 circlePointRadius = 0.9;
-  U32 originPointColumnIndex = pngPixels->width / 2;
-  U32 originPointRowIndex = pngPixels->height / 2;
-  U32 columnViewDimensionMagnitude = pngPixels->width - originPointColumnIndex - 1;
-  U32 rowViewDimensionMagnitude = pngPixels->height - originPointRowIndex - 1;
-  for (U32 pointIndex = 0; pointIndex < circlePointCount; pointIndex++)
+  IEEE64 viewCenterX = 0;
+  IEEE64 viewCenterY = 0;
+  IEEE64 viewMagnitude = 1;
+  IEEE64 viewLength = 2 * viewMagnitude;
+  IEEE64 viewLeftBoundsX = viewCenterX - viewMagnitude;
+  IEEE64 viewRightBoundsX = viewCenterX + viewMagnitude; // viewLeftBoundsX + viewLength
+  IEEE64 viewTopBoundsY = viewCenterY + viewMagnitude;
+  IEEE64 viewBottomBoundsY = viewCenterY - viewMagnitude; // viewTopBoundsY - viewLength
+  U32 circleCellCount = 12;
+  IEEE64 circleRadius = 0.9;
+  IEEE64 circleOriginX = 0;
+  IEEE64 circleOriginY = 0;
+  IEEE64 circleCellMagnitude = 0.05;
+  // U32 pixelsCenterColumnIndex = pngPixels->width / 2;
+  // U32 pixelsCenterRowIndex = pngPixels->height / 2;
+  // U32 viewColumnDimensionMagnitude = pngPixels->width - viewCenterColumnIndex - 1;
+  // U32 viewRowDimensionMagnitude = pngPixels->height - viewCenterRowIndex - 1;
+  for (U32 cellIndex = 0; cellIndex < circleCellCount; cellIndex++)
   {
-    IEEE64 pointAngle =
-      2 * M_PI / circlePointCount * pointIndex;
-    IEEE64 pointCos =
-      cos(pointAngle);
-    IEEE64 pointSin =
-      sin(pointAngle);
-    U32 pointColumnIndex =
-      (U32)(circlePointRadius * columnViewDimensionMagnitude * pointCos) + originPointColumnIndex;
-    U32 pointRowIndex =
-      (U32)(circlePointRadius * rowViewDimensionMagnitude * pointSin) + originPointRowIndex;
-    currentPixelChannels_ptr =
-      atPixelsDataPixelChannels(
-        pngPixels,
-        pointColumnIndex,
-        pointRowIndex);
-    currentPixelChannels_ptr->red = 0;
-    currentPixelChannels_ptr->green = 0;
-    currentPixelChannels_ptr->blue = 0;
+    IEEE64 circleCellAngle =
+      2 * M_PI / circleCellCount * cellIndex;
+    IEEE64 circleCellCos =
+      cos(circleCellAngle);
+    IEEE64 circleCellSin =
+      sin(circleCellAngle);
+    IEEE64 circleCellOriginX =
+      circleRadius * circleCellCos + circleOriginX;
+    IEEE64 circleCellOriginY =
+      circleRadius * circleCellSin + circleOriginY;
+    IEEE64 circleCellLeftX =
+      circleCellOriginX - circleCellMagnitude;
+    IEEE64 circleCellRightX =
+      circleCellOriginX + circleCellMagnitude;
+    IEEE64 circleCellTopY =
+      circleCellOriginY + circleCellMagnitude;
+    IEEE64 circleCellBottomY =
+      circleCellOriginY - circleCellMagnitude;
+    if (circleCellRightX < viewLeftBoundsX || circleCellLeftX > viewRightBoundsX || circleCellBottomY > viewTopBoundsY || circleCellTopY < viewBottomBoundsY)
+    {
+      continue;
+    }
+    IEEE64 viewCellLeftX =
+      circleCellLeftX < viewLeftBoundsX ? viewLeftBoundsX : circleCellLeftX;
+    IEEE64 viewCellRightX =
+      circleCellRightX > viewRightBoundsX ? viewRightBoundsX : circleCellRightX;
+    IEEE64 viewCellTopY =
+      circleCellTopY < viewTopBoundsY ? viewTopBoundsY : circleCellTopY;
+    IEEE64 viewCellBottomY =
+      circleCellBottomY > viewTopBoundsY ? viewTopBoundsY : circleCellBottomY;
+    U32 viewCellLeftColumnIndex = 
+      (viewCellLeftX - viewLeftBoundsX) / viewLength * pixelsWidth;
+    U32 viewCellRightColumnIndex = 
+      (viewCellRightX - viewLeftBoundsX) / viewLength * pixelsWidth;
+    // U32 viewCellTopRowIndex = 
+    //   (viewCellTop - view) / viewLength * pixelsWidth;
+    // U32 viewCellBottomRowIndex = 
+    //   (viewCellRightX - viewLeftBoundsX) / viewLength * pixelsWidth;
+    printf("%d %d\n", viewCellLeftColumnIndex, viewCellRightColumnIndex);
+    // U32 viewCellTopRowIndex = -1todo;
+    // U32 viewCellBottomRowIndex = -1todo;
+    //   (U32)(circlePointRadius * viewColumnDimensionMagnitude * pointCos) + viewCenterColumnIndex;
+    // U32 pointRowIndex =
+    //   (U32)(circlePointRadius * viewRowDimensionMagnitude * pointSin) + viewCenterRowIndex;
+    // currentPixelChannels_ptr =
+    //   atPixelsDataPixelChannels(
+    //     pngPixels,
+    //     pointColumnIndex,
+    //     pointRowIndex);
+    // currentPixelChannels_ptr->red = 0;
+    // currentPixelChannels_ptr->green = 0;
+    // currentPixelChannels_ptr->blue = 0;
   }
   encodeRgb8bitPngPixels(
     pngEncoding,
